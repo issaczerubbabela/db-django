a128091ca946125dfabfbc0fa17c77bc492a6c3f
Issac Zerubbabel A <126003111@sastra.ac.in>
2025-07-18 08:03:25 +0530

    Fix ports

--- backend/fastapi_app.py
+++ backend/fastapi_app.py
@@ -421,4 +421,4 @@ async def bulk_delete_automations(air_ids: List[str]):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
